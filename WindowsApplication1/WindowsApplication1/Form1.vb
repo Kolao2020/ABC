db335f63150ae0114acbdfc77a6cb69c17ba4179
Kolao2020 <67090174+Kolao2020@users.noreply.github.com>
2020-06-18 16:52:27 +0700

    revert
    ss

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
 
+<<<<<<< HEAD
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("111")
     End Sub
+=======
+>>>>>>> parent of f167d5b... l3
 End Class
