efbb14872c84df0c3a3f9c85bf27f78673d7eaa7
Kolao2020 <67090174+Kolao2020@users.noreply.github.com>
2020-06-18 15:38:22 +0700

    Update Form4.Designer.vb
    add label
    
    Co-Authored-By: kolao2020 <kolao2020@users.noreply.github.com>

--- git_tst/Form4.Designer.vb
+++ git_tst/Form4.Designer.vb
@@ -22,16 +22,29 @@ Partial Class Form4
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form4"
         Me.Text = "Form4"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
